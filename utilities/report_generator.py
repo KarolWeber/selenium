30f64affff6258052668d3276f6230752d13a7fa
Karol <weber.karol.86@gmail.com>
2024-06-04 18:54:39 +0200

    add() report generator

---
+++ utilities/report_generator.py
@@ -0,0 +1,7 @@
+from allure_combine import combine_allure
+
+"""
+alluredir=allure_automation
+allure generate allure_automation
+"""
+combine_allure(r"C:\Projects\selenium\allure-report", ignore_utf8_errors=True)
