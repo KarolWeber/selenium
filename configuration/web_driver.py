aa04c0ddf3a1b3739f0e219f4ae76b8b49787f15
Karol <weber.karol.86@gmail.com>
2024-06-05 10:39:46 +0200

    add() driver --headless

--- configuration/web_driver.py
+++ configuration/web_driver.py
@@ -6,6 +6,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 def web_driver():
     service = Service(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-gpu")
     driver = webdriver.Chrome(service=service, options=options)
     driver.maximize_window()
     return driver
